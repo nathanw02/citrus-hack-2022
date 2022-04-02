2faf9bb16de4f701e25cfae99290fa90bf7572c7
nathanw1510 <nathanw1510@gmail.com>
2022-04-02 15:38:21 -0700

    game

--- app/main.py
+++ app/main.py
@@ -20,6 +20,7 @@ def index():
 def connection(id):
     newSession = Session(id)
     sessions[id] = newSession
+    print(newSession.animal)
 
 @socketio.on('disconnect')
 def disconnect(id):
@@ -31,9 +32,17 @@ def guess(data):
     id = data['session']
 
     session = sessions[id]
-    
-    if session.guesses == MAX_GUESSES:
+
+    animalGuess = data['guess']
+
+    if session.guesses >= MAX_GUESSES:
         return emit('gameOver', {'data': session.animal})
+
+    if animalGuess not in animals:
+        return emit('error', {'error': 'Invalid input'})
+    
+    if animalGuess == session.animal:
+        return emit('win', {'data': 'Correct'})
 
     emit('matchResponse', session.match(data['guess']))
 
@@ -59,17 +68,30 @@ class Session:
 
         correctList = list(animalCorrect.values())
         guessList = list(animalGuess.values())
-        for i in range(15):
+        for i in range(17):
+            if i == 15:
+                continue
+
             if i == 12: #check leg count
                 if correctList[i] == guessList[i]:
                     result.append([int(correctList[i]), True])
                 else:
                     result.append([int(guessList[i]), False])
+            
+            elif i == 16: #check class type
+                if correctList[i] == guessList[i]:
+                    result.append([correctList[i], True])
+                else:
+                    result.append([guessList[i], False])
 
-            elif correctList[i] == guessList[i]: #normal check
-                result.append(1)
+            elif correctList[i] == guessList[i] and correctList[i] == 'true':
+                result.append([1, 1])
+
+            elif correctList[i] == guessList[i] and correctList[i] == 'false':
+                result.append([1, 0])
+
             else:
-                result.append(0)
+                result.append([0, 0])
         
         return {'match': result}
 
--- app/static/js/socket.js
+++ app/static/js/socket.js
@@ -1,11 +1,103 @@
 var socket = io();
+
+
+let attributes = ['hair', 'feather', 'milk', 'egg', 'airborne', 'aquatic', 'predator', 'toothed', 'backbone', 'breathes', 'venomous', 'fins', 'legs', 'tail', 'domestic']
     
+
+let inputs = document.getElementsByTagName('input')
+let currentInput = 0
+
 socket.on('connect', function() {
     socket.emit('send id', socket.id);
 });
 
+socket.on('win', function(data) {
+    console.log(data);
+    inputs[currentInput].disabled = true;
+    //do something
+
+    //display win
+
+    //display animal
+    //fun fact
+    //endangered not yes
+
+});
+
 socket.on('matchResponse', function(match) {
-    console.log(match)
+
+    matchArray = match.match;
+
+    for(let i = 0; i < matchArray.length; i++) {
+        let ptag = document.createElement('p');
+        let text;
+        if(i == 12) {
+            if(matchArray[i][1] == true) {
+                ptag.setAttribute('style', 'display: inline; color:greenyellow;');
+                
+            }else{
+                ptag.setAttribute('style', 'display: inline; color:gray;');
+            }
+
+            text = document.createTextNode(`${matchArray[i][0]} ${attributes[i]}    `);
+        
+        }else if(i == 15) {
+            if(matchArray[i][1] == true) {
+                ptag.setAttribute('style', 'display: inline; color:greenyellow;');
+                
+            }else{
+                ptag.setAttribute('style', 'display: inline; color:gray;');
+            }
+
+            text = document.createTextNode(`${matchArray[i][0]}    `);
+        
+        }else{
+            if(matchArray[i][0] == 1) {
+                if(matchArray[i][1] == 1) {
+                    ptag.setAttribute('style', 'display: inline; color:greenyellow;');
+                }else{
+                    ptag.setAttribute('style', 'display: inline; color:gold;');
+                }
+            }else{
+                ptag.setAttribute('style', 'display: inline; color:gray;');
+            }
+            text = document.createTextNode(`${attributes[i]}    `);
+        }
+
+        ptag.appendChild(text);
+        let matchId = `match-${currentInput}`;
+        let element = document.getElementById(matchId);
+        element.appendChild(ptag);
+
+    }
+    
+    inputs[currentInput].disabled = true;
+    currentInput++;
+    inputs[currentInput].disabled = false;
+    
+    /*
+     <p style="display: inline; color:greenyellow">hair</p>
+    <p style="display: inline; color: gray">feather</p>
+    <p style="display: inline; color:greenyellow">milk</p>
+    <p style="display: inline; color: greenyellow">egg</p>
+    <p style="display: inline; color:greenyellow">airborne</p>
+    <p style="display: inline; color: gray">aquatic</p>
+    <p style="display: inline; color:greenyellow">preadator</p>
+    <p style="display: inline; color: greenyellow">toothed</p>
+    <p style="display: inline; color:greenyellow">backbone</p>
+    <p style="display: inline; color: gray">breathes</p>
+    <p style="display: inline; color:greenyellow">venomous</p>
+    <p style="display: inline; color: greenyellow">fins</p>
+    <p style="word-spacing: 2px;display: inline; color:greenyellow">2 legs</p>
+    <p style="display: inline; color: gray">tail</p>
+    <p style="display: inline; color:greenyellow">domestic</p>
+    <p style="display: inline; color:gray">bird</p>
+    */
+
+});
+
+socket.on('error', function(err){
+    console.log(err);
 });
 
 function guess(animal){
