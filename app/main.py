f632ce52e33d68e42f678f591fb08d14616b3879
nathanw1510 <nathanw1510@gmail.com>
2022-04-02 13:08:44 -0700

    socket flask

--- app/main.py
+++ app/main.py
@@ -4,7 +4,12 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy dog'
+app.config['CORS_HEADERS'] = 'Content-Type'
 socketio = SocketIO(app)
+
+MAX_GUESSES = 10
+sessions = {}
 
 with open('citrus-hack-2022\\app\\animals.json') as json_file:
     animals = json.load(json_file)
@@ -13,12 +18,28 @@ with open('citrus-hack-2022\\app\\animals.json') as json_file:
 def index():
     return render_template('index.html')
 
+@socketio.on('send id')
+def connection(id):
+    newSession = Session(id)
+    sessions[id] = newSession
 
+@socketio.on('disconnect')
+def disconnect(id):
+    del sessions[id]
 
+@socketio.on('guess')
+def guess(data):
+    print('data:', data)
+    id = data['session']
 
+    session = sessions[id]
+    
+    if session.guesses == MAX_GUESSES:
+        return emit('gameOver', {'data': session.animal})
 
+    emit('matchResponse', session.match(data['guess']))
 
-
+    session.guesses += 1
 
 class Session:
     def __init__(self, id):
@@ -31,13 +52,29 @@ class Session:
         return random.choice(list(animals))
 
     def match(self, guess):
-        animalGuess = animals[guess]
-        result = {key: [val, animalGuess[key]] for key, val in animals[self.animal].items() if key in animalGuess}
-        print(result)
+        if guess == self.animal:
+            return {'match': 'correct'}
 
+        animalCorrect = animals[self.animal]
+        animalGuess = animals[guess]
+        result = []
+
+        correctList = list(animalCorrect.values())
+        guessList = list(animalGuess.values())
+        for i in range(15):
+            if i == 12: #check leg count
+                if correctList[i] == guessList[i]:
+                    result.append([int(correctList[i]), True])
+                else:
+                    result.append([int(guessList[i]), False])
+
+            elif correctList[i] == guessList[i]: #normal check
+                result.append(1)
+            else:
+                result.append(0)
+        
+        return {'match': result}
 
 if __name__ == '__main__':
-    #socketio.run(app, debug=True)
-    a = Session(123)
-    print(a.animal)
+    socketio.run(app, debug=True)
 
--- app/static/js/socket.js
+++ app/static/js/socket.js
@@ -1,7 +1,14 @@
 var socket = io();
     
-
 socket.on('connect', function() {
-    socket.send('send id', socket.id);
+    socket.emit('send id', socket.id);
+    
 });
 
+socket.on('matchResponse', function(match) {
+
+});
+
+function guess(animal){
+    socket.emit('guess', {'session': socket.id, 'guess': animal});
+}
